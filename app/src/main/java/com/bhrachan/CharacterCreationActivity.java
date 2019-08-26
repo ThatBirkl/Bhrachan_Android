9502cce1d1e457ec6e53c2aa64aaa7573f9b2dc7
ThatBirkl <m.birkl@adito.de>
2019-08-26 18:57:22 +0200

    .

--- app/src/main/java/com/bhrachan/CharacterCreationActivity.java
+++ app/src/main/java/com/bhrachan/CharacterCreationActivity.java
@@ -3,6 +3,7 @@ package com.bhrachan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -20,7 +21,8 @@ public class CharacterCreationActivity extends AppCompatActivity
             return false;
 
         skillPointsLeft--;
-        lbl_points.setText(skillPointsLeft);
+
+        lbl_points.setText("" + skillPointsLeft);
 
         return true;
     }
@@ -28,7 +30,7 @@ public class CharacterCreationActivity extends AppCompatActivity
     private void UncheckSkill()
     {
         skillPointsLeft++;
-        lbl_points.setText(skillPointsLeft);
+        lbl_points.setText("" + skillPointsLeft);
     }
 
     @Override
@@ -39,6 +41,7 @@ public class CharacterCreationActivity extends AppCompatActivity
         skillPointsLeft = 5;
 
         lbl_points = (TextView) findViewById(R.id.lbl_skillPoints);
+        lbl_points.setText("" + skillPointsLeft);
 
         Button btn_ok = (Button)findViewById(R.id.btn_ok);
         btn_ok.setOnClickListener(new View.OnClickListener() {
@@ -56,13 +59,42 @@ public class CharacterCreationActivity extends AppCompatActivity
             }
         });
 
-        CheckBox chk_skill_arcane = (CheckBox)findViewById(R.id.chk_skill_arcane);
-        chk_skill_arcane.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener chk_listener = new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 boolean checked = ((CheckBox) view).isChecked();
+
+                if(checked)
+                {
+                    CheckSkill();
+                }
+                else
+                {
+                    UncheckSkill();
+                }
             }
-        });
+        };
+
+        ((CheckBox)findViewById(R.id.chk_skill_axe)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_arcane)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_alchemy)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_bow)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_crafting)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_cooking)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_fauna)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_flora)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_instant)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_instincts)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_lockpicking)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_lore)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_manipulation)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_materialization)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_medical)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_restoration)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_ritual)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_shield)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_staff)).setOnClickListener(chk_listener);
+        ((CheckBox)findViewById(R.id.chk_skill_sword)).setOnClickListener(chk_listener);
+
     }
 }
